7f354934c432d305293c31e4f60c80bda9745d51
MBergmann <bergmann99@gmail.com>
2019-12-09 11:29:56 -0600

    Adding starter of Dashboard, includes table of passenger data

---
+++ Dashboard/assets/python/data-html.py
@@ -0,0 +1,10 @@
+# Dependencies and Setup
+import pandas as pd
+
+# File to Load
+file_to_load = "titanic.csv"
+
+# Read Purchasing File and store into Pandas data frame
+data = pd.read_csv(file_to_load)
+
+data.head()
