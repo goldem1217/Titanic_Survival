72da8900069a346e4b6baf4f10a63e5b000e6df6
Emily Goldner <eg12700@gmail.com>
2019-12-14 16:12:40 -0600

    age plot finished

--- Age_Plot/age_plot.js
+++ Age_Plot/age_plot.js
@@ -2,42 +2,53 @@ var svgWidth = 1000;
 var svgHeight = 660;
 
 var chartMargin = {
-  top: 100,
-  right: 30,
+  top: 50,
+  right: 50,
   bottom: 30,
-  left: 30
+  left: 50
 };
 
 var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
 var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
 
+var fp1 = 'ages.csv'
+var fp2 = 'binned_ages.csv'
+
 var tip1 = d3.tip()
 .attr('class', 'd3-tip')
 .offset([-20, 0])
 .html(function(d) {
-  return (`<strong>Age: ${d.age}<strong><h6>${d.percent_s}% Survived</h6>`);
-})
+  return (`<strong>Age: ${d.age}<strong><hr>${d.percent_s}% Survived`);
+});
 var tip2 = d3.tip()
 .attr('class', 'd3-tip')
 .offset([-10, 0])
 .html(function(d) {
-  return (`<strong>Age: ${d.age}<strong><h6>${d.survivors} Survived</h6>`);
-})
+  return (`<strong>Age: ${d.age}<strong><hr>${d.survivors} Survived`);
+});
 
 ///////////////////////////////////////////////////////////////////////
 
 d3.select("body")
 .append("div")
-.attr("class", "container")
+.attr("class", "special")
 .html(
-  "<select id='options'>\
-  <option value='ages.csv' selected='selected'>Ages</option>\
-  <option value='binned_ages.csv'>Age Groups</option>\
-  </select>\
-  <select id='options2'>\
-  <option value='ages.csv' selected='selected'>Ages</option>\
-  <option value='binned_ages.csv'>Age Groups</option>\
-  </select><br>");
+  `<div class=row>
+    <div class = col-2>
+      <h4 class = 'writing'>Percent Survived:</h4>
+      <select id='options' class = 'select'>
+        <option value=${fp1} selected='selected'>By Age</option>
+        <option value=${fp2}>By Age Group</option>
+      </select>
+    </div>
+    <div class = col-2>
+      <h4 class = 'writing'>Number of Survivors:</h4>
+      <select id='options2' class = 'select'>
+        <option value=${fp1} selected='selected'>By Age</option>
+        <option value=${fp2}>By Age Group</option>
+      </select>
+    </div>
+  </div>`);
 
 var svg = d3
   .select("div")
@@ -46,13 +57,13 @@ var svg = d3
   .attr("width", svgWidth);
 
 var chartGroup = svg.append("g")
-  .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`)
+  .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
 
 ///////////////////////////////////////////////////////////////////////
 
 function chart1(csv){
   d3.csv(csv).then(function(ageData) {
-    if(csv === 'ages.csv'){
+    if(csv === `${fp1}`){
       ageData.forEach(function(data) {
           data.age = +data.age;
           data.total_onboard = +data.total_onboard;
@@ -62,7 +73,7 @@ function chart1(csv){
           data.percent_d = +data.percent_d;        
         });
     };
-    if(csv === 'binned_ages.csv'){
+    if(csv === `${fp2}`){
       ageData.forEach(function(data) {
           data.age = data.age;
           data.total_onboard = +data.total_onboard;
@@ -72,7 +83,6 @@ function chart1(csv){
           data.percent_d = +data.percent_d;        
         });
     };
-
   var barSpacing = 3;
   var scaleY = 5;
 
@@ -80,7 +90,7 @@ function chart1(csv){
 
   var ageLabels = ageData.map(function(d) { 
     return d.age });
-
+  
   var yScale = d3.scaleLinear()
   .domain([0,100])
   .range([chartHeight, 0]);
@@ -98,16 +108,11 @@ function chart1(csv){
   .attr("class", "axis")
   .call(xAxis);
   
-
   chartGroup.append("g")
   .attr("class", "axis")
   .call(yAxis);
 
-  //d3.select('.axis')
-  //.attr("stroke","#E04836")
-  //.attr("stroke-width","6")
-  //.attr("opacity",".6");
-
+  console.log(ageData.percent_s);
 
   var barsGroup = chartGroup.selectAll(".bar")
   .data(ageData)
@@ -120,6 +125,7 @@ function chart1(csv){
   .attr("x", (d, i) => i * (barWidth + barSpacing))
   .attr("y", d => chartHeight - yScale(100-d.percent_s));
 
+  console.log(ageData.age);
   
   chartGroup.call(tip1);
 
@@ -129,12 +135,13 @@ function chart1(csv){
         tip1.hide(d);
 
       });
+      
   });
 };
 
 function chart2(csv) {
 d3.csv(csv).then(function(ageData) {
-  if(csv === 'ages.csv'){
+  if(csv === fp1){
     ageData.forEach(function(data) {
         data.age = +data.age;
         data.total_onboard = +data.total_onboard;
@@ -144,7 +151,7 @@ d3.csv(csv).then(function(ageData) {
         data.percent_d = +data.percent_d;        
       });
   };
-  if(csv === 'binned_ages.csv'){
+  if(csv === fp2){
     ageData.forEach(function(data) {
         data.age = data.age;
         data.total_onboard = +data.total_onboard;
@@ -217,7 +224,7 @@ d3.select('#options')
   var sect = document.getElementById("options");
   var section = sect.options[sect.selectedIndex].value;
 
-  d3.selectAll("rect").remove();
+  d3.selectAll(".bar").remove();
   d3.selectAll(".axis").remove();
 
   console.log(section)
@@ -233,17 +240,15 @@ d3.select('#options2')
   var sect = document.getElementById("options2");
   var section = sect.options[sect.selectedIndex].value;
 
-  d3.selectAll("rect").remove();
+  d3.selectAll(".bar").remove();
   d3.selectAll(".axis").remove();
 
   console.log(section)
   console.log("as whole numbers")
 
-
   return chart2(section)
 
-  
   });
   
 
-chart1(`ages.csv`)
+chart1(`${fp1}`);
