d0026fea97ea830b03b959a865a02573d17c859d
sdelozier15 <52932991+sdelozier15@users.noreply.github.com>
2019-12-17 19:04:43 -0600

    Update app.py

--- API/app.py
+++ API/app.py
@@ -1,20 +1,20 @@
 import json
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
+ 
 import cleaner
     
-# We create a Flask app
 app = Flask(__name__)
 
-# We establish a Flask route so that we can serve HTTP traffic on that route 
+@app.route('/')
+def index():
+	return render_template('index.html')
+
 @app.route('/titanic/<name>')
 def titanic(name):
     with open('db/titanic_1308.json', 'r') as jsonfile:
         file_data = json.loads(jsonfile.read())
     # We can then find the data for the requested name and send it back as json
     return json.dumps(file_data[name])
-
-# Get setup so that if we call the app directly (and it isn't being imported elsewhere)
-# it will then run the app with the debug mode as True
-# More info - https://docs.python.org/3/library/__main__.html
+ 
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(debug=True)
